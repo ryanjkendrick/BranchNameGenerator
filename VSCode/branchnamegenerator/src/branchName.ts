ef03f8d42845e86d14274f59ac78a027c073fb76
Ryan Kendrick <ryan@kendrickfamily.info>
2020-12-02 14:50:46 +0000

    Fix create branch funtionality

--- VSCode/branchnamegenerator/src/branchName.ts
+++ VSCode/branchnamegenerator/src/branchName.ts
@@ -1,12 +1,11 @@
 import * as vscode from 'vscode';
 
 export function showBranchName(branchName: string) {
-    let createBranch = "Create Branch";
     let copy = "Copy";
     const name = branchName.toLowerCase();
     const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
 
-    if (gitExtension !== undefined && gitExtension !== null && gitExtension.getAPI(1).repositories.length > 0) {
+    if (gitExtension !== undefined && gitExtension !== null && gitExtension.getAPI(1).repositories.length > 0) { // In a git repo
         let createBranch = "Create Branch";
 
         vscode.window.showInformationMessage(name, copy, createBranch)
@@ -15,29 +14,23 @@ export function showBranchName(branchName: string) {
                 vscode.env.clipboard.writeText(name);
             }
             else if (selection == createBranch) {
-                const api = gitExtension.getAPI(1);
-                const repo = api.repositories[0];
-                
-                repo.createBranch(name, true);
+                vscode.env.clipboard.writeText(name);
+
+                vscode.window.showInformationMessage("Do you want to checkout this branch?", "Yes", "No")
+                .then(checkoutSelection => {
+                    const api = gitExtension.getAPI(1);
+                    const repo = api.repositories[0];
+
+                    repo.createBranch(name, checkoutSelection === "Yes");
+                });
             }
         });
     }
-    else {
+    else { // Is not in a git repo
         vscode.window.showInformationMessage(name, copy)
         .then(selection => {
             if (selection === copy) {
                 vscode.env.clipboard.writeText(name);
-            } else if (selection == createBranch) {
-                vscode.env.clipboard.writeText(name);
-
-                const gitExtension = vscode.extensions.getExtension('vscode.git')?.exports;
-                const api = gitExtension.getAPI(1);
-                const repo = api.repositories[0];
-
-                vscode.window.showInformationMessage("Do you want to checkout this branch?", "Yes", "No")
-                .then(checkoutSelection => {
-                    repo.createBranch(name, checkoutSelection === "Yes");
-                });
             }
         });
     }
