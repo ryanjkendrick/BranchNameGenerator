a1d71edd7928ca4e93675c2a4d82e9d0945ba49a
Ryan Kendrick <ryan@kendrickfamily.info>
2020-12-02 14:05:23 +0000

    Delete out dir

--- VSCode/branchnamegenerator/out/validateInput.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function validateInput(value) {
-    var regex = new RegExp('[A-Z]+-\d+-.+');
-    return regex.test(value);
-}
-exports.validateInput = validateInput;
-//# sourceMappingURL=validateInput.js.map
